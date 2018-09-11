049fb8f3fd7c8fb3fb6db4940ce1fbb52a2c9651
David Ma <davidma415@gmail.com>
2018-09-11 02:18:46 -0400

    Fniished step 3

--- ajax.js
+++ ajax.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // step 1
+  function ajaxStep12() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    });
+  }
 
-  /* Your code goes here */
+  // step 2
+  var request1 = document.querySelector('.step2');
+  request1.addEventListener('click', ajaxStep12);
+
+  // step 3
+  function ajaxStep3() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    }).done(function (responseData) {
+      console.log(responseData);
+      var section3456 = document.querySelector('#step3456');
+      section3456.append(responseData);
+    });
+  }
+  var request2 = document.querySelector('.button3456');
+  request2.addEventListener('click', ajaxStep3)
 
 });
